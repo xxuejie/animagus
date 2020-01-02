0ddd6c22ba3d1a89e06f8464477e6a3a4444d7eb
Xuejie Xiao <xxuejie@gmail.com>
2020-01-02 06:17:09 +0000

    Implement fork switching

--- pkg/indexer/indexer.go
+++ pkg/indexer/indexer.go
@@ -2,8 +2,10 @@ package indexer
 
 import (
 	"bytes"
+	"compress/gzip"
 	"context"
 	"encoding/binary"
+	"encoding/json"
 	"fmt"
 	"log"
 	"time"
@@ -107,12 +109,49 @@ func (i *Indexer) Run() error {
 			lastBlockHash = lastBlock[8:]
 		}
 
-		req := graphql.NewRequest(`
+		block, err := i.queryBlock(blockToFetch)
+		if err != nil {
+			return err
+		}
+		if block == nil {
+			time.Sleep(time.Second)
+			continue
+		}
+
+		revert := lastBlockHash != nil && (!bytes.Equal(block.Header.ParentHash[:], lastBlockHash))
+		if revert {
+			if blockToFetch == 0 {
+				return fmt.Errorf("Nowhere to revert!")
+			}
+			err = i.revertBlock(blockToFetch - 1)
+			if err != nil {
+				return err
+			}
+			continue
+		}
+
+		// In the optimal path, we keep only one Redis connection
+		commands := &commandBuffer{}
+		err = i.indexBlock(*block, commands)
+		if err != nil {
+			return err
+		}
+		err = commands.execute(redisConn)
+		if err != nil {
+			return err
+		}
+		log.Printf("Indexed block %x, block number %d", block.Header.Hash, block.Header.Number)
+	}
+}
+
+func (i *Indexer) queryBlock(blockNumber uint64) (*rpctypes.BlockView, error) {
+	req := graphql.NewRequest(`
 query($blockNumber: String) {
   getBlock(number: $blockNumber) {
     header {
       parent_hash
       hash
+      number
     }
     transactions {
       hash
@@ -146,91 +185,133 @@ query($blockNumber: String) {
   }
 }
 `)
-		req.Var("blockNumber", rpctypes.Uint64(blockToFetch).EncodeToString())
-		var response getBlockResponse
-		err = i.graphqlClient.Run(context.Background(), req, &response)
-		if err != nil {
-			return err
-		}
-		if response.GetBlock == nil {
-			time.Sleep(time.Second)
-			continue
-		}
-		block := *response.GetBlock
-
-		if lastBlockHash != nil && (!bytes.Equal(block.Header.ParentHash[:], lastBlockHash)) {
-			return fmt.Errorf("TODO: implement forking")
-		}
-
-		commands := &commandBuffer{}
-		for _, tx := range block.Transactions {
-			for _, input := range tx.RawTransaction.Inputs {
-				if input.PreviousOutput.GraphqlCell != nil &&
-					input.PreviousOutput.GraphqlCellData != nil {
-					for _, valueContext := range i.values {
-						for queryIndex, query := range valueContext.Queries {
-							// TODO: cell data support
-							params, err := executeIndexingQuery(query,
-								input.PreviousOutput.GraphqlCell,
-								input.PreviousOutput.GraphqlCellData.Content)
-							if err != nil {
-								return err
-							}
-							if params != nil {
-								key, err := valueContext.IndexKey(queryIndex, params)
-								if err != nil {
-									return nil
-								}
-								commands.remove(key, input.PreviousOutput)
-							}
-						}
-					}
-				}
-			}
-
-			for outputIndex, output := range tx.RawTransaction.Outputs {
-				for _, valueContext := range i.values {
-					for queryIndex, query := range valueContext.Queries {
-						params, err := executeIndexingQuery(query, &output,
-							tx.RawTransaction.GraphqlCellsData[outputIndex].Content)
-						if err != nil {
-							return err
-						}
-						if params != nil {
-							key, err := valueContext.IndexKey(queryIndex, params)
-							if err != nil {
-								return err
-							}
-							commands.insert(key, rpctypes.OutPoint{
-								TxHash: tx.Hash,
-								Index:  rpctypes.Uint32(outputIndex),
-							})
-						}
-					}
-				}
-			}
-		}
-		commands.do("SET", fmt.Sprintf("BLOCK:%d:HASH", blockToFetch), block.Header.Hash[:])
-		lastBlock = make([]byte, 40)
-		binary.LittleEndian.PutUint64(lastBlock, blockToFetch)
-		copy(lastBlock[8:], block.Header.Hash[:])
-		commands.do("SET", "LAST_BLOCK", lastBlock)
-		err = commands.execute(redisConn)
-		if err != nil {
-			return err
-		}
-		log.Printf("Indexed block %x, block number %d", block.Header.Hash, blockToFetch)
+	req.Var("blockNumber", rpctypes.Uint64(blockNumber).EncodeToString())
+	var response getBlockResponse
+	err := i.graphqlClient.Run(context.Background(), req, &response)
+	if err != nil {
+		return nil, err
 	}
+	return response.GetBlock, nil
+}
+
+func (i *Indexer) indexBlock(block rpctypes.BlockView, commands *commandBuffer) error {
+	var err error
+	for _, tx := range block.Transactions {
+		for _, input := range tx.RawTransaction.Inputs {
+			if input.PreviousOutput.GraphqlCell != nil &&
+				input.PreviousOutput.GraphqlCellData != nil {
+				err = i.processCell(input.PreviousOutput.GraphqlCell,
+					input.PreviousOutput.GraphqlCellData.Content,
+					input.PreviousOutput,
+					true,
+					commands)
+				if err != nil {
+					return err
+				}
+			}
+		}
+
+		for outputIndex, output := range tx.RawTransaction.Outputs {
+			err = i.processCell(&output,
+				tx.RawTransaction.GraphqlCellsData[outputIndex].Content,
+				rpctypes.OutPoint{
+					TxHash: tx.Hash,
+					Index:  rpctypes.Uint32(outputIndex),
+				},
+				false,
+				commands)
+			if err != nil {
+				return err
+			}
+		}
+	}
+	blockNumber := uint64(block.Header.Number)
+	blockHashKey := fmt.Sprintf("BLOCK:%d:HASH", blockNumber)
+	commands.do("SET", blockHashKey, block.Header.Hash[:])
+	lastBlock := make([]byte, 40)
+	binary.LittleEndian.PutUint64(lastBlock, blockNumber)
+	copy(lastBlock[8:], block.Header.Hash[:])
+	commands.do("SET", "LAST_BLOCK", lastBlock)
+
+	revertKey := fmt.Sprintf("BLOCK:%d:REVERT_COMMANDS", blockNumber)
+	commands.setRevertKey(revertKey)
+	commands.revertDo("DEL", blockHashKey)
+	if blockNumber > 0 {
+		previousBlock := make([]byte, 40)
+		binary.LittleEndian.PutUint64(previousBlock, blockNumber-1)
+		copy(previousBlock[8:], block.Header.ParentHash[:])
+		commands.revertDo("SET", "LAST_BLOCK", previousBlock)
+	} else {
+		commands.revertDo("DEL", "LAST_BLOCK")
+	}
+	commands.revertDo("DEL", revertKey)
+
+	return nil
+}
+
+func (i *Indexer) revertBlock(blockNumber uint64) error {
+	conn := i.redisPool.Get()
+	defer conn.Close()
+
+	revertKey := fmt.Sprintf("BLOCK:%d:REVERT_COMMANDS", blockNumber)
+	revertData, err := redis.Bytes(conn.Do("GET", revertKey))
+	if err != nil {
+		return err
+	}
+
+	var revertCommands []command
+	gzipReader, err := gzip.NewReader(bytes.NewReader(revertData))
+	if err != nil {
+		return err
+	}
+	decoder := json.NewDecoder(gzipReader)
+	err = decoder.Decode(&revertCommands)
+	if err != nil {
+		return err
+	}
+
+	conn.Send("MULTI")
+	for _, command := range revertCommands {
+		conn.Send(command.Name, command.Args...)
+	}
+	_, err = conn.Do("EXEC")
+	return err
+}
+
+func (i *Indexer) processCell(cell *rpctypes.CellOutput, cellData *rpctypes.Raw, outPoint rpctypes.OutPoint, insert bool, commands *commandBuffer) error {
+	for _, valueContext := range i.values {
+		for queryIndex, query := range valueContext.Queries {
+			// TODO: cell data support
+			params, err := executeIndexingQuery(query, cell, cellData)
+			if err != nil {
+				return err
+			}
+			if params != nil {
+				key, err := valueContext.IndexKey(queryIndex, params)
+				if err != nil {
+					return err
+				}
+				if insert {
+					commands.insert(key, outPoint)
+				} else {
+					commands.remove(key, outPoint)
+				}
+			}
+		}
+	}
+	return nil
 }
 
 type command struct {
-	name string
-	args []interface{}
+	Name string        `json:"n"`
+	Args []interface{} `json:"a"`
 }
 
 type commandBuffer struct {
-	commands []command
-	err      error
+	commands       []command
+	revertCommands []command
+	revertKey      string
+	err            error
 }
 
 func (c *commandBuffer) do(commandName string, args ...interface{}) {
@@ -238,9 +319,26 @@ func (c *commandBuffer) do(commandName string, args ...interface{}) {
 		return
 	}
 	c.commands = append(c.commands, command{
-		name: commandName,
-		args: args,
+		Name: commandName,
+		Args: args,
 	})
+}
+
+func (c *commandBuffer) revertDo(commandName string, args ...interface{}) {
+	if c.err != nil {
+		return
+	}
+	c.revertCommands = append(c.revertCommands, command{
+		Name: commandName,
+		Args: args,
+	})
+}
+
+func (c *commandBuffer) setRevertKey(key string) {
+	if c.err != nil {
+		return
+	}
+	c.revertKey = key
 }
 
 func (c *commandBuffer) insert(key string, outPoint rpctypes.OutPoint) {
@@ -250,6 +348,7 @@ func (c *commandBuffer) insert(key string, outPoint rpctypes.OutPoint) {
 	var buffer bytes.Buffer
 	c.err = outPoint.SerializeToCore(&buffer)
 	c.do("SADD", key, buffer.Bytes())
+	c.revertDo("SREM", key, buffer.Bytes())
 }
 
 func (c *commandBuffer) remove(key string, outPoint rpctypes.OutPoint) {
@@ -259,17 +358,35 @@ func (c *commandBuffer) remove(key string, outPoint rpctypes.OutPoint) {
 	var buffer bytes.Buffer
 	c.err = outPoint.SerializeToCore(&buffer)
 	c.do("SREM", key, buffer.Bytes())
+	c.revertDo("SADD", key, buffer.Bytes())
 }
 
 func (c *commandBuffer) execute(conn redis.Conn) error {
 	if c.err != nil {
 		return c.err
 	}
+
+	if len(c.revertKey) == 0 {
+		return fmt.Errorf("Revert key is missing!")
+	}
+	var buf bytes.Buffer
+	gzipWriter := gzip.NewWriter(&buf)
+	encoder := json.NewEncoder(gzipWriter)
+	err := encoder.Encode(c.revertCommands)
+	if err != nil {
+		return err
+	}
+	err = gzipWriter.Close()
+	if err != nil {
+		return err
+	}
+
 	conn.Send("MULTI")
 	for _, command := range c.commands {
-		conn.Send(command.name, command.args...)
+		conn.Send(command.Name, command.Args...)
 	}
-	_, err := conn.Do("EXEC")
+	conn.Send("SET", c.revertKey, buf.Bytes())
+	_, err = conn.Do("EXEC")
 	return err
 }
 
