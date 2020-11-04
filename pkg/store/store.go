be296c5f5db659aecd3d4c0afbf1ea573c071fab
classicalliu <classicalliu@gmail.com>
2020-11-04 17:23:15 +0800

    Using prefix scan for set operations

--- pkg/store/store.go
+++ pkg/store/store.go
@@ -1,8 +1,6 @@
 package store
 
 import (
-	"bytes"
-	"encoding/gob"
 	"fmt"
 
 	badger "github.com/dgraph-io/badger/v2"
@@ -81,32 +79,18 @@ func (c *Client) Del(key string) error {
 	return err
 }
 
+func skeyPrefix(key string) []byte {
+	return append([]byte(key), []byte(":")...)
+}
+
+func skey(key string, value []byte) []byte {
+	return append(skeyPrefix(key), value...)
+}
+
 func Sadd(txn *badger.Txn, key string, value []byte) error {
-	keyByte := []byte(key)
-	originValue, errr := Get(txn, keyByte)
-	if errr != nil {
-		return errr
-	}
+	newKey := skey(key, value)
 
-	var bytesArr [][]byte
-	if len(originValue) != 0 {
-		bytesArr, errr = Decode(originValue)
-		if errr != nil {
-			return errr
-		}
-	}
-
-	if contains(bytesArr, value) {
-		return nil
-	}
-
-	bytesArr = append(bytesArr, value)
-	resultBytes, errr := Encode(bytesArr)
-	if errr != nil {
-		return errr
-	}
-
-	errr = txn.Set(keyByte, resultBytes)
+	errr := txn.Set(newKey, nil)
 	return errr
 }
 
@@ -119,27 +103,9 @@ func (c *Client) Sadd(key string, value []byte) error {
 }
 
 func Srem(txn *badger.Txn, key string, value []byte) error {
-	keyByte := []byte(key)
-	originValue, errr := Get(txn, keyByte)
-	if errr != nil {
-		return errr
-	}
+	newKey := skey(key, value)
 
-	bytesArr, errr := Decode(originValue)
-	if errr != nil {
-		return errr
-	}
-	if !contains(bytesArr, value) {
-		return nil
-	}
-
-	bytesArr = sliceRemove(bytesArr, value)
-	resultBytes, errr := Encode(bytesArr)
-	if errr != nil {
-		return errr
-	}
-
-	errr = txn.Set(keyByte, resultBytes)
+	errr := txn.Delete(newKey)
 	return errr
 }
 
@@ -152,21 +118,24 @@ func (c *Client) Srem(key string, value []byte) error {
 }
 
 func Sget(txn *badger.Txn, key string) ([][]byte, error) {
-	keyByte := []byte(key)
-	var bytesArr [][]byte
-	originValue, errr := Get(txn, keyByte)
-	if errr != nil {
-		return nil, errr
+	var values [][]byte
+
+	opts := badger.DefaultIteratorOptions
+	opts.PrefetchValues = false
+	it := txn.NewIterator(opts)
+	defer it.Close()
+	prefix := skeyPrefix(key)
+	prefixLen := len(prefix)
+	for it.Seek(prefix); it.ValidForPrefix(prefix); it.Next() {
+		item := it.Item()
+		k := item.Key()
+		value := k[prefixLen:]
+		value2 := make([]byte, len(value))
+		copy(value2, value)
+		values = append(values, value2)
 	}
 
-	if len(originValue) != 0 {
-		bytesArr, errr = Decode(originValue)
-		if errr != nil {
-			return nil, errr
-		}
-	}
-
-	return bytesArr, errr
+	return values, nil
 }
 
 func (c *Client) Sget(key string) ([][]byte, error) {
@@ -177,39 +146,6 @@ func (c *Client) Sget(key string) ([][]byte, error) {
 		return errr
 	})
 	return bytesArr, err
-}
-
-func Spop(txn *badger.Txn, key string) ([]byte, error) {
-	keyByte := []byte(key)
-
-	values, err := Sget(txn, key)
-	if err != nil {
-		return nil, err
-	}
-	if len(values) == 0 {
-		return nil, nil
-	}
-	firstValue := values[0]
-
-	newValues := sliceRemoveIdx(values, 0)
-
-	resultBytes, err := Encode(newValues)
-	if err != nil {
-		return nil, err
-	}
-
-	err = txn.Set(keyByte, resultBytes)
-	return firstValue, err
-}
-
-func (c *Client) Spop(key string) ([]byte, error) {
-	var result []byte = nil
-	err := c.DB.Update(func(txn *badger.Txn) error {
-		var errr error
-		result, errr = Spop(txn, key)
-		return errr
-	})
-	return result, err
 }
 
 func Do(txn *badger.Txn, command string, args ...interface{}) error {
@@ -236,41 +172,4 @@ func Do(txn *badger.Txn, command string, args ...interface{}) error {
 	}
 
 	return fmt.Errorf("command %v not found", command)
-}
-
-func Encode(value [][]byte) ([]byte, error) {
-	var result bytes.Buffer
-	encoder := gob.NewEncoder(&result)
-	err := encoder.Encode(value)
-	return result.Bytes(), err
-}
-
-func Decode(value []byte) ([][]byte, error) {
-	from := bytes.NewBuffer(value)
-	var result [][]byte
-	decoder := gob.NewDecoder(from)
-	err := decoder.Decode(&result)
-	return result, err
-}
-
-func contains(s [][]byte, e []byte) bool {
-	for _, a := range s {
-		if bytes.Equal(a, e) {
-			return true
-		}
-	}
-	return false
-}
-
-func sliceRemoveIdx(slice [][]byte, idx int) [][]byte {
-	return append(slice[0:idx], slice[idx+1:]...)
-}
-
-func sliceRemove(slices [][]byte, toRemove []byte) [][]byte {
-	for idx, value := range slices {
-		if bytes.Equal(value, toRemove) {
-			return sliceRemoveIdx(slices, idx)
-		}
-	}
-	return slices
 }
